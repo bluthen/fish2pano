b4111985bb7ada0c528ec6d92eea79eb4513151b
Russell Valentine <russell.valentine@gmail.com>
2023-11-10 13:00:35 -0600

    Cleaned up formating for findcircle.py

--- findcircle.py
+++ findcircle.py
@@ -4,9 +4,10 @@ import numpy as np
 from scipy import optimize
 import json
 
+
 def calc_r(contour_t, xc, yc):
     """ calculate the distance of each 2D points from the center (xc, yc) """
-    return np.sqrt((contour_t[0]-xc)**2 + (contour_t[1]-yc)**2)
+    return np.sqrt((contour_t[0] - xc) ** 2 + (contour_t[1] - yc) ** 2)
 
 
 def gen_f2(contour_t):
@@ -15,10 +16,12 @@ def gen_f2(contour_t):
     :param contour_t:
     :return:
     """
+
     def f2(c):
         """ calculate the algebraic distance between the data points and the mean circle centered at c=(xc, yc) """
         r_i = calc_r(contour_t, *c)
         return r_i - r_i.mean()
+
     return f2
 
 
@@ -30,29 +33,28 @@ def circle_least_squares(contour):
     """
     # http://scipy-cookbook.readthedocs.io/items/Least_Squares_Circle.html
 
-    #If concave
+    # If concave
     contour_shape = contour.shape
     contour_r = contour.T
-    #contour_r = np.reshape(contour, [contour_shape[0], contour_shape[2]]).T
+    # contour_r = np.reshape(contour, [contour_shape[0], contour_shape[2]]).T
     center, ier = optimize.leastsq(gen_f2(contour_r), (3888, 3888))
     r_i2 = calc_r(contour_r, center[0], center[1])
     r_2 = np.mean(r_i2)
-    r_residu2 = np.sum((r_i2 - r_2)**2.0)
+    r_residu2 = np.sum((r_i2 - r_2) ** 2.0)
 
-    #If convex
+    # If convex
     contour_shape = contour.shape
     contour_r = contour.T
-    #contour_r = np.reshape(contour, [contour_shape[0], contour_shape[2]]).T
+    # contour_r = np.reshape(contour, [contour_shape[0], contour_shape[2]]).T
     center_b, ier = optimize.leastsq(gen_f2(contour_r), (0, 0))
     r_i2 = calc_r(contour_r, center_b[0], center_b[1])
     r_2_b = np.mean(r_i2)
-    r_residu2 = np.sum((r_i2 - r_2)**2.0)
+    r_residu2 = np.sum((r_i2 - r_2) ** 2.0)
 
     if r_2_b < r_2:
         return r_2_b, center_b
     else:
         return r_2, center
-
 
 
 def main():
@@ -69,25 +71,25 @@ def main():
     def mouse_cb(event, x, y, flags, param):
         if not nomore and event == cv2.EVENT_LBUTTONDOWN:
             print('point', x, y)
-            points.append((x,y))
-            cv2.circle(img_raw, (x,y), 3, (0,0,255), -1)
+            points.append((x, y))
+            cv2.circle(img_raw, (x, y), 3, (0, 0, 255), -1)
 
     cv2.namedWindow('image')
     cv2.setMouseCallback('image', mouse_cb)
 
     img_raw = cv2.imread(imgfile)
     while True:
-       cv2.imshow('image', img_raw)
-       k = cv2.waitKey(20) & 0xFF
-       if k == 27:
-           break
+        cv2.imshow('image', img_raw)
+        k = cv2.waitKey(20) & 0xFF
+        if k == 27:
+            break
     nomore = True
     points = np.array(list(map(np.array, points)))
-    #print(points)
+    # print(points)
     r, center = circle_least_squares(points)
     print(json.dumps({"radius": r, "center": list(center)}))
-    center = list(map(lambda x: int(x+0.5), center))
-    r = int(r+0.5)
+    center = list(map(lambda x: int(x + 0.5), center))
+    r = int(r + 0.5)
     cv2.circle(img_raw, center, r, (0, 255, 0), 1)
     cv2.circle(img_raw, center, 3, (0, 255, 0), -1)
     cv2.imshow('image', img_raw)
